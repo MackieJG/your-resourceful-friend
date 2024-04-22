e5e3969a6683d3d4caed7f6cf24d730cc779fbf4
unknown <joshuamackie10@googlemail.com>
2024-04-22 16:45:05 +0100

    linked client to server on project generator

--- src/app/project-generator/page.tsx
+++ src/app/project-generator/page.tsx
@@ -1,15 +1,45 @@
-import { Button } from "@/components/ui/button"
-import { Dices } from "lucide-react"
+"use client";
 
-const ProjectGenerator = () => {
-  return (
-    <div className="flex flex-1 items-center justify-center text-center flex-col gap-11">
-      <Button><Dices className="mr-2 h-4 w-4" /> Generate</Button>
-      <h1 className="text-4xl md:text-7xl font-bold">
-        Build a <span className= "text-primary">Discord Bot</span> for a <span className="text-primary">Valheim Server</span>
-      </h1>
-      </div>
-  )
+import { Button } from "@/components/ui/button";
+import { Dices } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface ProjectDataProps {
+  business: string;
+  concept: string;
 }
 
-export default ProjectGenerator
+const ProjectGenerator = () => {
+  const apiUrl = "http://localhost:8080";
+  const [projectData, setProjectData] = useState<ProjectDataProps | null>(null);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network Response was not Ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProjectData(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-1 items-center justify-center text-center flex-col gap-11">
+      <Button>
+        <Dices className="mr-2 h-4 w-4" /> Generate
+      </Button>
+      <h1 className="text-4xl md:text-7xl font-bold">
+        Build <span className="text-primary">{projectData?.concept}</span> for{" "}
+        <span className="text-primary">{projectData?.business}</span>
+      </h1>
+    </div>
+  );
+};
+
+export default ProjectGenerator;
