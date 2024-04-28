9725b6792c2f766ab31d484bbe75fc6c41b93e1d
unknown <joshuamackie10@googlemail.com>
2024-04-28 20:36:20 +0100

    created project generator function including mobile navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,16 +7,16 @@ import { supabase } from "@/supabase";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-import LogoLight from "../assets/logo-light.svg"
-import LogoDark from "../assets/logo-dark.svg"
+import LogoLight from "../assets/logo-light.svg";
+import LogoDark from "../assets/logo-dark.svg";
 import { useTheme } from "next-themes";
 
 export const googleFormLink =
   "https://docs.google.com/forms/d/e/1FAIpQLSdG3EZWq1jFut0jkrsugBLZrDD8p5yxBdeqpiusLCQCBiJCjw/viewform?usp=sf_link";
-export default function NavBar() {
 
+export default function NavBar() {
   const { resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
   const [categories, setCategories] = useState<any>();
 
   const getAllCategories = async () => {
@@ -30,21 +30,30 @@ export default function NavBar() {
   };
 
   useEffect(() => {
-    setMounted(true)
+    setMounted(true);
     getAllCategories();
-  }, [])
+  }, []);
 
   if (!mounted) {
-    return null
+    return null;
   }
-  
+
   return (
     <nav className="flex py-4 items-center gap-8 justify-between">
       <div className="flex items-center gap-8">
         <Link href="/" className="max-w-sm">
-          <Image src={resolvedTheme === "dark" ? LogoDark : LogoLight} alt="JuniorDevResources Logo" />
+          <Image
+            src={resolvedTheme === "dark" ? LogoDark : LogoLight}
+            alt="JuniorDevResources Logo"
+          />
         </Link>
-        <Link href="project-generator" className={buttonVariants({ variant: "ghost" }) + " hidden sm:inline-flex"}>Project Generator</Link>
+        <Link
+          href="project-generator"
+          className={
+            buttonVariants({ variant: "ghost" }) + " hidden sm:inline-flex"
+          }>
+          Project Generator
+        </Link>
       </div>
       <div className="flex items-center gap-4">
         <ModeToggle />
